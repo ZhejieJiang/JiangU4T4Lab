7ad82529b31895867af68a9b78fc63108d6e39d2
Zhejie Jiang <zhejiej@nycstudents.net>
2022-11-17 08:08:36 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        for (int j = 6; j >= 0; j--){
+            for (int i = j; i >= 0; i--)
+            {
+                System.out.print( i + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
